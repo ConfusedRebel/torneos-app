f55db3439a0b1a118bb27f850d696bb15c43af21
Francisco Javier <fbottazzini@uade.edu.ar>
2025-10-27 19:24:48 -0300

    trying to build the apk

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
+console.log('✅ URL:', process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log('✅ KEY exists:', !!process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
+
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
